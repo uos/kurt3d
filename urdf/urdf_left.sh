ddeba12569888d1a3d12cdd3280f38a2b8b5d7df
robot11 <robot@robot11.funky.uni-osnabrueck.de>
2014-04-11 15:43:28 +0200

    deleted unused files

--- urdf/urdf_left.sh
+++
@@ -1,5 +0,0 @@
-pfad=$PWD
-rosdep install joint_state_publisher
-rosdep install urdf_tutorial
-roscd urdf_tutorial
-roslaunch urdf_tutorial display.launch model:=$pfad/left_cam.urdf gui:=true
--- urdf/urdf_right.sh
+++
@@ -1,5 +0,0 @@
-pfad=$PWD
-rosdep install joint_state_publisher
-rosdep install urdf_tutorial
-roscd urdf_tutorial
-roslaunch urdf_tutorial display.launch model:=$pfad/right_cam.urdf gui:=true
