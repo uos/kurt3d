c46f049913d7ced5eda39e24e58abc2955d8da94
robot11 <robot@robot11.funky.uni-osnabrueck.de>
2014-04-11 16:21:34 +0200

    merged the pololu_driver into the uos_3dscanner package

---
+++ src/USBInterface.cpp
@@ -0,0 +1,383 @@
+/* Copyright (C) 2011 Uni Osnabrück
+ * This file is part of the LAS VEGAS Reconstruction Toolkit,
+ *
+ * LAS VEGAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LAS VEGAS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+
+/**
+ * USBInterface.cpp
+ *
+ *  @date Feb 28, 2013
+ *  @author Thomas Wiemann
+ */
+
+#include "USBInterface.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int USBPololuInterface::MOVE_TARGET_TIMEOUT = 1000;
+int USBPololuInterface::MOVE_TARGET_MAX_TRYS = 1000;
+
+USBPololuInterface::USBPololuInterface(uint16_t v_id, uint16_t p_id)
+: m_device(0), m_context(0), m_deviceHandle(0)
+{
+    // Create new libusb context
+    libusb_init(&m_context);
+
+    // Open device
+    struct libusb_device **devs;
+    struct libusb_device_descriptor info;
+
+    // Get list of devices and counts
+    int count = libusb_get_device_list(m_context,&devs);
+
+    // Walk the list, read descriptors, and dump some output from each
+    int pololuCount = 0;
+    for (int i = 0; i < count;i++)
+    {
+        libusb_get_device_descriptor(devs[i],&info);
+
+        if(info.idVendor == v_id && info.idProduct == p_id)
+        {
+            pololuCount++;
+            m_device = devs[i];
+        }
+    }
+
+    // Warn if several boards are connected
+    if(pololuCount > 1)
+    {
+        cout << "USBPololuInterface: Detected " << pololuCount << "boards. Using last detected one." << endl;
+    }
+
+    // Try to open device
+    int ret;
+    if(m_device)
+    {
+        ret = libusb_open(m_device, &m_deviceHandle);
+    }
+
+    if(ret < 0)
+    {
+        cout << "USBPololuInterface: Unable to open device." << endl;
+    }
+
+}
+
+
+
+string USBPololuInterface::getErrorDescription(int code)
+{
+    switch(code)
+    {
+    case -1:
+        return "I/O error.";
+    case -2:
+        return "Invalid parameter.";
+    case -3:
+        return "Access denied.";
+    case -4:
+        return "Device does not exist.";
+    case -5:
+        return "No such entity.";
+    case -6:
+        return "Busy.";
+    case -7:
+        return "Timeout.";
+    case -8:
+        return "Overflow.";
+    case -9:
+        return "Pipe error.";
+    case -10:
+        return "System call was interrupted.";
+    case -11:
+        return "Out of memory.";
+    case -12:
+        return "Unsupported/unimplemented operation.";
+    case -99:
+        return "Other error.";
+    default:
+        return "Unknown error code.";
+    };
+}
+
+void USBPololuInterface::controlTransfer(char requestType, char request, ushort value, ushort index)
+{
+    if(m_deviceHandle)
+    {
+        int result = libusb_control_transfer(
+                m_deviceHandle,
+                requestType,
+                request,
+                value,
+                index,
+                0, 0, 5000);
+
+        if(result < 0)
+        {
+            cout << "USBPololuInterface::controlTransfer(): " << getErrorDescription(result) << endl;
+        }
+    }
+    else
+    {
+        cout << "USBPololuInterface::controlTransfer(): Device not open." << endl;
+    }
+}
+
+void USBPololuInterface::controlTransfer(
+        char requestType,
+        char request,
+        ushort value,
+        ushort index,
+        unsigned char* data,
+        ushort length)
+{
+    if(m_deviceHandle)
+    {
+        int result = libusb_control_transfer(
+                m_deviceHandle,
+                requestType,
+                request,
+                value,
+                index,
+                data, length, 5000);
+
+        if(result < 0)
+        {
+            cout << "USBPololuInterface::controlTransfer(): " << getErrorDescription(result) << endl;
+        }
+    }
+    else
+    {
+        cout << "USBPololuInterface::controlTransfer(): Device not open." << endl;
+    }
+}
+
+
+uint8_t USBPololuInterface::channelToPort(uint8_t channel)
+{
+    if (channel <= 3)
+    {
+        return channel;
+    }
+    else if (channel < 6)
+    {
+        return uint8_t(channel + 2);
+    }
+    cout << "USBPololuInterface::channelToPort(): Unsupported channel: << " << channel << endl;
+    return 0;
+}
+
+
+void USBPololuInterface::setRawParameter(ushort parameter, ushort value, int bytes)
+{
+    ushort index = (ushort)((bytes << 8) + parameter); // high bytes = # of bytes
+    controlTransfer(0x40, uscRequest::REQUEST_SET_PARAMETER, value, index);
+}
+
+
+uscParameter USBPololuInterface::specifyServo(uscParameter p, uint8_t servo)
+{
+    return (uscParameter)((uint8_t)(p) + servo * servoParameterBytes);
+}
+
+uint8_t USBPololuInterface::normalSpeedToExponentialSpeed(ushort normalSpeed)
+{
+    ushort mantissa = normalSpeed;
+    uint8_t exponent = 0;
+
+    while (true)
+    {
+        if (mantissa < 32)
+        {
+            // We have reached the correct representation.
+            return (uint8_t)(exponent + (mantissa << 3));
+        }
+
+        if (exponent == 7)
+        {
+            // The number is too big to express in this format.
+            return 0xFF;
+        }
+
+        // Try representing the number with a bigger exponent.
+        exponent += 1;
+        mantissa >>= 1;
+    }
+}
+
+void USBPololuInterface::setUscSettings(uscSettings settings)
+{
+    // Serial settings are not supported
+    setRawParameter(uscParameter::PARAMETER_SCRIPT_DONE, (ushort)(settings.scriptDone ? 1 : 0), sizeof(ushort));
+    setRawParameter(uscParameter::PARAMETER_SERVOS_AVAILABLE, settings.servosAvailable, sizeof(uint8_t));
+    setRawParameter(uscParameter::PARAMETER_SERVO_PERIOD, settings.servoPeriod, sizeof(uint8_t));
+
+
+    uint8_t ioMask = 0;
+    uint8_t outputMask = 0;
+    uint8_t* channelModeBytes = new uint8_t[6]{0,0,0,0,0,0};
+
+    std::list<channelSetting>::iterator it;
+
+    int i = 0;
+    for(it = settings.channelSettings.begin(); it != settings.channelSettings.end(); it++)
+    {
+        channelSetting setting = *it;
+
+
+        if (setting.mode == ChannelMode::Input || setting.mode == ChannelMode::Output)
+        {
+            ioMask |= (uint8_t)(1 << channelToPort(i));
+        }
+
+        if (setting.mode == ChannelMode::Output)
+        {
+            outputMask |= (uint8_t)(1 << channelToPort(i));
+        }
+
+
+
+        // Make sure that HomeMode is "Ignore" for inputs.  This is also done in
+        // fixUscSettings.
+        HomeMode correctedHomeMode = setting.homeMode;
+        if (setting.mode == ChannelMode::Input)
+        {
+            correctedHomeMode = HomeMode::Ignore;
+        }
+
+        // Compute the raw value of the "home" parameter.
+        ushort home;
+        if (correctedHomeMode == HomeMode::Off) home = 0;
+        else if (correctedHomeMode == HomeMode::Ignore) home = 1;
+        else home = setting.home;
+
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_HOME, i), home, sizeof(ushort));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_MIN, i), (ushort)(setting.minimum / 64), sizeof(ushort));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_MAX, i), (ushort)(setting.maximum / 64), sizeof(ushort));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_NEUTRAL, i), setting.neutral, sizeof(uint16_t));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_RANGE, i), (ushort)(setting.range / 127), sizeof(ushort));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_SPEED, i), normalSpeedToExponentialSpeed(setting.speed), sizeof(uint16_t));
+        setRawParameter(specifyServo(uscParameter::PARAMETER_SERVO0_ACCELERATION, i), setting.acceleration, sizeof(uint8_t));
+    }
+
+    setRawParameter(uscParameter::PARAMETER_IO_MASK_C, ioMask, sizeof(uint8_t));
+    setRawParameter(uscParameter::PARAMETER_OUTPUT_MASK_C, outputMask, sizeof(uint8_t));
+
+}
+
+
+
+
+
+void USBPololuInterface::getServoStatus(int servo, servoStatus& status)
+{
+    // Alloc memory for maestro board status struct and 6 servo status structs
+    int packet_size = sizeof(maestroStatus) + 6 * sizeof(servoStatus);
+    unsigned char* buffer = new unsigned char[packet_size];
+
+    // Request data
+    controlTransfer(0xC0, REQUEST_GET_VARIABLES, 0, 0, buffer, packet_size);
+
+    // Parse buffer data into servo status
+    unsigned char* p = buffer;
+    servoStatus servos[6];
+    for(int i = 0; i < 6; i++)
+    {
+        servos[i] = *(servoStatus*)(p + sizeof(maestroStatus) + sizeof(servoStatus) * i);
+    }
+
+    // Set output data
+    status.position     = servos[servo].position / 4;
+    status.target       = servos[servo].target / 4;
+    status.speed        = servos[servo].speed;
+    status.acceleration = servos[servo].acceleration;
+}
+
+void USBPololuInterface::setTarget(int servo, ushort value)
+{
+    // Target values have to be set in micro seconds. Conversion factor
+    // from absolute values to mic. sec. is '4' (see Pololu SDK)
+    controlTransfer(0x40, REQUEST_SET_TARGET, 4.0 * value, servo);
+}
+
+void USBPololuInterface::moveToTarget(int servo, ushort target)
+{
+    setTarget(servo, target);
+    servoStatus status;
+
+    // Wait until servo has reached target position
+    int i = 0;
+    do
+    {
+        usleep(MOVE_TARGET_TIMEOUT);
+        getServoStatus(servo, status);
+        i++;
+    }
+    while(status.position != target && i < MOVE_TARGET_MAX_TRYS);
+
+    // Check for timeout
+    if(i == MOVE_TARGET_MAX_TRYS)
+    {
+        cout << "USBPololuInterface::moveToTarget(): Timeout. Target position " << target << " out of servo range?" << endl;
+    }
+}
+
+void USBPololuInterface::moveToTarget(int servo, ushort target, ushort time)
+{
+    servoStatus status;
+    getServoStatus(servo, status);
+
+    // Calculate steps to target and timeout
+    int steps   = std::fabs(status.position - target);
+    int timeout = time * 1000 / steps;
+
+    // Decide if we have to increment or decrement
+    int initialPosition = status.position;
+    int factor = 1;
+    if(initialPosition > target) factor *= -1;
+
+    // Move to target
+    for(int i = 0; i <= steps; i++)
+    {
+        setTarget(servo, initialPosition + factor * i);
+        usleep(timeout);
+    }
+
+}
+
+void USBPololuInterface::setSpeed(int servo, ushort value)
+{
+    controlTransfer(0x40, REQUEST_SET_SERVO_VARIABLE, value, servo);
+}
+
+USBPololuInterface::~USBPololuInterface()
+{
+    // Close device
+    if(m_deviceHandle)
+    {
+        libusb_close(m_deviceHandle);
+    }
+
+    // Release context if valid
+    if(m_context)
+    {
+        libusb_exit(m_context);
+    }
+
+
+}
---
+++ src/servo_node.cpp
@@ -0,0 +1,196 @@
+#include "ros/ros.h"
+#include "uos_3dscanner/Servo_Command.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/JointState.h"
+#include "uos_3dscanner/servo_control.h"
+#include <sstream>
+#include "USBInterface.h"
+#include "clock.h"
+#include <iostream>
+#include <boost/thread.hpp>
+#define _USE_MATH_DEFINES
+
+#define RAD(GRAD) ((GRAD * (float)M_PI) / (float)180)
+
+#define SERVO_RANGE 1000
+#define SERVO_MIN 1000
+
+static ros::Publisher state_pub;
+
+static sensor_msgs::JointState currentState;
+
+static double min_pos[5] =
+{
+    RAD(-50.0),//fertig
+    RAD(-65.0),//fertig
+    RAD(-65.0),//fertig
+    RAD(-50.0),//fertig
+    RAD(-65.0) //fertig
+};
+static double max_pos[5] =
+{
+    RAD(60.0),
+    RAD(45.0),
+    RAD(45.0),
+    RAD(60.0),
+    RAD(45.0)
+};
+
+static std::string jointNames[5] =
+{ "drehobjekt_1_to_balken_1",
+  "servo_1_a_to_wing_1_a",
+  "servo_1_b_to_wing_1_b",
+  "servo_2_a_to_wing_2_a",
+  "servo_2_b_to_wing_2_b"};
+
+
+boost::mutex& mut()
+{
+    static boost::mutex m;
+    return m;
+}
+
+void setJointState(int channel, double angle)
+{
+    currentState.header.stamp = ros::Time::now();
+
+
+    if(channel == 2 || channel == 4|| channel == 0) angle *= -1.0;
+
+    currentState.position[channel] = angle;
+}
+
+void moveServo(int channel, double angle, int speed, bool secure)
+{
+    boost::lock_guard<boost::mutex> _(mut());
+
+    USBPololuInterface usb;
+
+    uscSettings settings;
+    usb.setUscSettings(settings);
+
+    usb.setSpeed(channel, speed);
+
+    double target = (double)angle;
+    target -= min_pos[channel];
+
+    target /=  (max_pos[channel] - min_pos[channel]);
+
+    target *= SERVO_RANGE;
+
+    target += SERVO_MIN;
+
+    if(secure)
+    {
+        usb.moveToTarget(channel, target);
+    }
+    else
+    {
+        usb.setTarget(channel, target);
+    }
+    // As the board position information seems biased we just assume the requested angle was reached.
+    setJointState(channel, angle);
+
+    state_pub.publish(currentState);
+
+    std::cout << "Reported servo status: " << std::endl;
+}
+
+
+void servoCallback(const uos_3dscanner::servo_control::ConstPtr& req)
+{
+     ROS_INFO("A servo movement was requested by topic");
+     ROS_INFO("Channel: [%i] Target: [%f] Speed: [%i]",req->channel,req->angle,req->speed);
+     if(req->channel == 0) return;
+
+     double target = req->angle;
+
+     if(req->angle < min_pos[req->channel])
+     {
+         ROS_INFO("Required Angle out of range!");
+         target = min_pos[req->channel];
+     }
+
+     if(req->angle  > max_pos[req->channel])
+     {
+         ROS_INFO("Required Angle out of range!");
+         target = max_pos[req->channel];
+     }
+
+     moveServo(req->channel, target, req->speed, false);
+
+}
+
+
+bool nod(uos_3dscanner::Servo_Command::Request  &req,
+         uos_3dscanner::Servo_Command::Response &res)
+{
+  ROS_INFO("A servo movement was requested by service");
+  ROS_INFO("Channel: [%i] Target: [%f] Speed: [%i]",req.channel,req.angle,req.speed);
+
+
+  double target = req.angle;
+
+  if(req.angle < min_pos[req.channel])
+  {
+      ROS_INFO("Required Angle out of range!");
+      target = min_pos[req.channel];
+  }
+
+  if(req.angle  > max_pos[req.channel])
+  {
+      ROS_INFO("Required Angle out of range!");
+      target = max_pos[req.channel];
+  }
+
+  moveServo(req.channel, target, req.speed, true);
+
+  res.finished = true;
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "servo_node");
+
+  ros::NodeHandle n;
+
+  // inits the service
+  ros::ServiceServer service = n.advertiseService("servo_node", nod);
+
+  // inits the publisher
+  state_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+
+  // inits the subscriber
+  ros::Subscriber sub = n.subscribe("servo_control", 5, servoCallback);
+
+
+  // init the current State
+  currentState = sensor_msgs::JointState();
+  currentState.name.resize(5);
+  currentState.position.resize(5);
+
+  // populate the current State with the values of the servos
+  for (int i = 0; i < 5; i++)
+  {
+      setJointState(i, 0.0);
+      currentState.name[i] = jointNames[i];
+  }
+
+
+  ROS_INFO("Ready to control servos.");
+
+  while(ros::ok())
+  {
+      {
+          boost::lock_guard<boost::mutex> _(mut());
+
+          currentState.header.stamp = ros::Time::now();
+          state_pub.publish(currentState);
+      }
+
+      ros::spinOnce();
+  }
+
+  return 0;
+}
