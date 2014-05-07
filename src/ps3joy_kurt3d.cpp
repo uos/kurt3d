3ee31a89b73d7efa4f1c631d6811b2516af3cff4
Benjamin Kisliuk <bekisliuk@uos.de>
2014-05-07 17:24:04 +0200

    Add ps3joy node

---
+++ src/ps3joy_kurt3d.cpp
@@ -0,0 +1,149 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include "uos_3dscanner/Scan.h"
+#include "pololu_driver/servo_control.h"
+
+#define RAD(GRAD) ((GRAD * (float)M_PI) / (float)180)
+
+double max_vel_x, max_rotational_vel;
+ros::Publisher vel_pub;
+double speed_multiplier;
+static bool blocked = false;
+static ros::ServiceClient client;
+ros::Publisher servo_pub;
+
+static double min_pos[5] =
+{
+    RAD(-50.0),//fertig
+    RAD(-65.0),//fertig
+    RAD(-65.0),//fertig
+    RAD(-50.0),//fertig
+    RAD(-65.0) //fertig
+};
+static double max_pos[5] =
+{
+    RAD(60.0),
+    RAD(45.0),
+    RAD(45.0),
+    RAD(60.0),
+    RAD(45.0)
+};
+
+void ps3joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist vel;
+  if (joy->buttons[8] == 1)   // check for full-speed button
+  {
+    speed_multiplier = 1.0;
+  }
+  else if (true)  // check if right analog stick was used to scale speed
+  {
+    speed_multiplier = 0.5 + (0.5 * joy->axes[3]);  // stick full front -> speed_multiplier = 1.0 , full back -> 0.0
+  }
+  else  // else use half max speed
+  {
+    speed_multiplier = 0.5;
+  }
+
+  // check if cross is used for steering
+  if (joy->buttons[4] || joy->buttons[5] || 
+      joy->buttons[6] || joy->buttons[7])
+  {
+    // note that every button of the cross (axes 4-7) generates 
+    // an output in [-1.0, 0.0]
+    vel.linear.x = max_vel_x * (joy->axes[4] * -1.0 + joy->axes[6]) * speed_multiplier;
+    vel.angular.z = max_rotational_vel * (joy->axes[7] * -1.0 + joy->axes[5]) * speed_multiplier;
+  }
+  // check if triangle is used for laser scanning
+  if (joy->buttons[12])
+  {
+     uos_3dscanner::Scan srv;
+
+     if (client.call(srv))
+     {
+       ROS_INFO("Scan finished");
+     }
+     else
+     {
+       ROS_ERROR("Failed to call laserscanner_node service");
+     }
+
+  }
+  if(joy->buttons[10])
+  {
+    int channel_1 = 2;
+    int channel_2 = 1;
+
+    pololu_driver::servo_control msg_x;
+    pololu_driver::servo_control msg_z;
+
+    double angle_x = (((joy->axes[2]*-1.0+1.0) / 2.0) * (max_pos[channel_1]-min_pos[channel_1])) + min_pos[channel_1];
+    double angle_z = (((joy->axes[3]*-1.0+1.0) / 2.0) * (max_pos[channel_2]-min_pos[channel_2])) + min_pos[channel_2];
+
+    msg_x.channel = channel_1;
+    msg_x.angle =  angle_x;
+    msg_x.speed = 0;
+
+    msg_z.channel = channel_2;
+    msg_z.angle =  angle_z;
+    msg_z.speed = 0;
+
+
+    servo_pub.publish(msg_x);
+    servo_pub.publish(msg_z);
+  }
+  if(joy->buttons[11])
+  {
+    int channel_1 = 4;
+    int channel_2 = 3;
+
+
+    pololu_driver::servo_control msg_x;
+    pololu_driver::servo_control msg_z;
+
+    double angle_x = (((joy->axes[2]*-1+1.0) / 2.0) * (max_pos[channel_1]-min_pos[channel_1])) + min_pos[channel_1];
+    double angle_z = (((joy->axes[3]+1.0) / 2.0) * (max_pos[channel_2]-min_pos[channel_2])) + min_pos[channel_2];
+
+
+    msg_x.channel = channel_1;
+    msg_x.angle =  angle_x;
+    msg_x.speed = 0;
+
+    msg_z.channel = channel_2;
+    msg_z.angle =  angle_z;
+    msg_z.speed = 0;
+
+
+    servo_pub.publish(msg_x);
+    servo_pub.publish(msg_z);
+
+  }
+  else  // use left analog stick
+  {
+    vel.linear.x = max_vel_x * joy->axes[1] * speed_multiplier;
+    vel.angular.z = max_rotational_vel * joy->axes[0] * speed_multiplier;
+  }
+  vel_pub.publish(vel);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ps3joy_kurt3d");
+
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_ns("~");
+
+  nh_ns.param("max_vel_x", max_vel_x, 1.5);
+  nh_ns.param("max_rotational_vel", max_rotational_vel, 3.0);
+
+  vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  ros::Subscriber ps3joy_sub = nh.subscribe("joy", 10, ps3joyCallback);
+
+  client = nh.serviceClient<uos_3dscanner::Scan>("laserscanner_node");
+
+  servo_pub = nh.advertise<pololu_driver::servo_control>("servo_control", 4);
+
+  ros::spin();
+}
+
